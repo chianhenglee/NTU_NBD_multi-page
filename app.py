84b2bd391cbfd8bb6fab41216de9347bec2b5b1b
Chian-Heng Lee <chianhenglee@gmail.com>
2018-11-18 20:31:41 +0800

    I moved everythin to old_complete_stuff and bring in the simplest multi-page app template for testing

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 import dash
+import os
 
-#app = dash.Dash()
-app = dash.Dash(__name__) # this is needed for adding local CSS files. (in assets folder)
+from flask import send_from_directory
+
+
+app = dash.Dash()
 server = app.server
-app.config.suppress_callback_exceptions = True
+app.config.supress_callback_exceptions = True
 
-server.secret_key = os.environ.get('secret_key', 'secret')
+external_css = [
+    'https://codepen.io/chriddyp/pen/bWLwgP.css',
+    '/static/base.css'
+]
+for css in external_css:
+    app.css.append_css({"external_url": css})
 
+
+@app.server.route('/static/<path:path>')
+def static_file(path):
+    static_folder = os.path.join(os.getcwd(), 'static')
+    return send_from_directory(static_folder, path)
---
+++ old_complete_stuff/app.py
@@ -0,0 +1,9 @@
+import dash
+
+#app = dash.Dash()
+app = dash.Dash(__name__) # this is needed for adding local CSS files. (in assets folder)
+server = app.server
+app.config.suppress_callback_exceptions = True
+
+#server.secret_key = os.environ.get('secret_key', 'secret')
+
