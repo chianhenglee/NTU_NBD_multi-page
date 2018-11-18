3f3bd206afdf2c47e94fef8d81bcedb2c6029ede
Chian-Heng Lee <chianhenglee@gmail.com>
2018-11-18 21:17:36 +0800

    modified Procfile

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ app = dash.Dash(__name__) # this is needed for adding local CSS files. (in asset
 server = app.server
 app.config.suppress_callback_exceptions = True
 
+
 #server.secret_key = os.environ.get('secret_key', 'secret')
 
