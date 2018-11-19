ee7e1ae070fd9ceecb5b66579d52719e823cf277
Chian-Heng Lee <chianhenglee@gmail.com>
2018-11-19 15:10:36 +0800

    moved the complete app back

--- index.py
+++ index.py
@@ -1,35 +1,53 @@
-from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
 from app import app
-from apps import app1, app2
+#from apps import app1, app2
+from apps import choropleth, lineplots #, hospital_data_visual2
+
 
 server = app.server
 
-
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
+    html.Div(id='page-content',
+             children=[
+                html.H3('This is the index page!'),
+                #html.H4(dcc.Link('Line plots', href='/apps/lineplots')),
+                #html.H4(dcc.Link('Choropleth', href='/apps/choropleth')),
+                #html.H4(dcc.Link('Treemaps', href='/apps/treemaps')),
+                #html.H4(dcc.Link('Subplots', href='/apps/subplots'))
+                #html.H4(dcc.Link('hospital_data_visual2', href='/apps/hospital_data_visual2'))
+                      ]),
 ])
+
 
 
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/apps/app1':
-         return app1.layout
-    elif pathname == '/apps/app2':
-         return app2.layout
+    print(pathname)
+    if pathname == '/apps/lineplots':
+        return lineplots.layout
+    elif pathname == '/apps/choropleth':
+        return choropleth.layout
+#    elif pathname =='apps/hospital_data_visual2':
+#        return hospital_data_visual2.layout
     elif pathname =='/':
         return html.Div([
                 #dcc.Location(id='url', refresh=False),
                 html.Div([
                     html.H3('This is the index page!!'),
-                    html.H4(dcc.Link('App1', href='/apps/app1')),
-                    html.H4(dcc.Link('App2', href='/apps/app2')),
+                    html.H4(dcc.Link('Line plots', href='/apps/lineplots')),
+                    html.H4(dcc.Link('Choropleth', href='/apps/choropleth')),
+                    html.H4(dcc.Link('Hospital_data_visual', href='/apps/hospital_data_visual2'))
                           ]),
                 ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
+
+
